bc77c53169c982737d2935d29491286fad1c1880
Nichole John Martinez <xin_martinez@yahoo.com>
2016-06-27 14:49:41 +0800

    Fix base view and add new css

--- resources/views/base.blade.php
+++ resources/views/base.blade.php
@@ -5,31 +5,36 @@
 	<title>Native Camping Site : @yield('title')</title>
 	<!-- DEFAULT STYLES -->
 	{!! Html::style('css/default-libs.min.css') !!}
+	<!-- ADDITIONAL STYLES -->
+	@yield('styles')
 </head>
 <body>
-	@yield('nav')
-	<div class="container">
-		@if (count($errors) > 0)
-			<div class="alert alert-danger">
-				<ul>
-					@foreach ($errors->all() as $error)
-						<li>{{ $error }}</li>
-					@endforeach
-				</ul>
-			</div>
-		@elseif (session('customError'))
-			<div class="alert alert-danger">
-				<p>{{ session('customError') }}</p>
-			</div>
-		@elseif (session('message'))
-			<div class="alert alert-success">
-				<p>{{ session('message') }}</p>
-			</div>
-		@endif
-
+	<div id="wrapper">
+		@yield('nav')
+		<div class="container">
+			@if (count($errors) > 0)
+				<div class="alert alert-danger">
+					<ul>
+						@foreach ($errors->all() as $error)
+							<li>{{ $error }}</li>
+						@endforeach
+					</ul>
+				</div>
+			@elseif (session('customError'))
+				<div class="alert alert-danger">
+					<p>{{ session('customError') }}</p>
+				</div>
+			@elseif (session('message'))
+				<div class="alert alert-success">
+					<p>{{ session('message') }}</p>
+				</div>
+			@endif
+		</div>
 		@yield('content')
 	</div>
 	<!-- DEFAULT SCRIPT -->
 	{!! Html::script('js/default-libs.min.js') !!}
+	<!-- ADDITIONAL SCRIPTS -->
+	@yield('scripts')
 </body>
 </html>
