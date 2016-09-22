11c687def3d18747bd65079b120b9e4eed821825
Nichole John Martinez <xinmartinez@gmail.com>
2016-09-22 13:25:12 +0800

    Add Onair model

---
+++ app/Onair.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Onair extends Model {
+	
+	protected $table = 'onairs';
+	
+	protected $fillable = array(
+		'teacher_id',              // Teacher's id
+		'student_id',              // Student's id
+		'status',                  // Status for lesson => 1: standby, 2: reserved, 3: lesson
+		'connect_flg',             // Connection flag => 1: connected, 0: not connected
+		'chat_hash',               // Chat hash for this lesson
+		'wait_start_time',         // Datetime start registered during standby
+		'wait_end_time',           // Datetime end during end of standby
+		'start_time',              // Datetime of start in lesson
+		'end_time',                // Datetime end of lesson
+		'lesson_type',             // Lesson type => 1: normal, 2:reserved
+		'web_rtc_type',            // webrtc type => 1: skyway, 2:other
+		'lesson_finish'            // lesson finish boolean => 1: finish, 0: not finish
+	);
+	
+}
